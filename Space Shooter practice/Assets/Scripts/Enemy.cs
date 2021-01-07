16b4ced3f0754b49a6c1cd05eb687d538b81b9dd
SkittishFeline <74646676+SkittishFeline@users.noreply.github.com>
2021-01-07 10:15:42 +0100

    Converted from 3D to 2D
    Converted prototype to the production stage

--- Space Shooter practice/Assets/Scripts/Enemy.cs
+++ Space Shooter practice/Assets/Scripts/Enemy.cs
@@ -27,7 +27,7 @@ public class Enemy : MonoBehaviour
         //change the position to teleport from the top of the sceen from 7.5f on the y axis
     }
 
-    void OnTriggerEnter(Collider other)
+    void OnTriggerEnter2D(Collider2D other)
     {
         Debug.Log("Hit: " + other.transform.name);
         if(other.tag == "Laser")
