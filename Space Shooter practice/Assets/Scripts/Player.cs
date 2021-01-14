241127931e12f32f9f1db48630debb7c6356366e
SkittishFeline <74646676+SkittishFeline@users.noreply.github.com>
2021-01-14 10:51:44 +0100

    Finished powerup spawning
    Created powerup
    Created animation for powerup
    Spawnmanager includes powerup
    Powerup has cooldown

--- Space Shooter practice/Assets/Scripts/Player.cs
+++ Space Shooter practice/Assets/Scripts/Player.cs
@@ -11,6 +11,8 @@ public class Player : MonoBehaviour
 
     private GameObject _laser;
     [SerializeField]
+    private GameObject _tripleShotPrefab;
+    [SerializeField]
 
     private float _fireRate = 0.5f;
 
@@ -19,6 +21,8 @@ public class Player : MonoBehaviour
 
     private int _lives = 3;
     private SpawnManager _spawnManager;
+    [SerializeField]
+    private bool _isTripleShotActive = false;
 
     // Start is called before the first frame update
     void Start()
@@ -107,8 +111,16 @@ public class Player : MonoBehaviour
     {
         Debug.Log("Space is pressed");
             _canFire = Time.time + _fireRate;
-            //create a clone on the player's position using the default rotation
-            Instantiate(_laser,transform.position + new Vector3(0,1f,0),Quaternion.identity);
+            if(_isTripleShotActive == true)
+            {
+                Instantiate(_tripleShotPrefab, transform.position,Quaternion.identity);
+            }
+            else
+            {
+                //create a clone on the player's position using the default rotation
+                Instantiate(_laser,transform.position + new Vector3(0,1f,0),Quaternion.identity);
+            }
+            
     }
 
     public void Damage()
@@ -122,5 +134,20 @@ public class Player : MonoBehaviour
                 _spawnManager.OnPlayerDeath();
                 Destroy(this.gameObject);
             }
+        }
+
+        public void ActivateTripleShot()
+        {
+            _isTripleShotActive = true;
+            //start coroutine when _IsTripleShotActive is true
+            StartCoroutine(TripleShotPowerDownRoutine());
+        }
+
+        IEnumerator TripleShotPowerDownRoutine()
+        {
+            //wait for 5 seconds
+            yield return new WaitForSeconds(5f);
+            //turn off triple shot
+            _isTripleShotActive = false;
         }
 }
